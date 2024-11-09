a5bceb352c3b6fb7a7d5a8c24e73c19039da9479
TheWolfLovers <thewolflovers12345@gmail.com>
2024-11-09 11:10:45 +0000

    CMD+ uploaded

---
+++ Alpha 0.1.6 dev/isolation/CMD+.py
@@ -0,0 +1,41 @@
+# Basic ass code improved!!
+# CMD+ Launcher 1.0
+
+#---------[ Modules ]---------#
+import os
+import random
+import sys
+import time
+import shutil
+import plyer
+import platform
+import subprocess
+from plyer import notification
+import winsound
+import tkinter
+import playsound
+notif = notification
+#-----------------------------#
+#check 1
+try:
+    # access directory "bin"
+    os.chdir("bin")
+    try:
+        subprocess.run(["python", "CMD+.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        notif.notify(
+            title="Error",
+            message=f"Error executing CMD+: {e}",
+            timeout=30
+        )
+    exit()
+        
+# Send a notif saying that it failed and output
+# Saying what the error is (most likely that the
+# directory doesn't exist
+except OSError as e:
+    notif.notify(
+        title = "Error",
+        message = f"Error from OS: {e}",
+        timeout=8)
+    exit()
